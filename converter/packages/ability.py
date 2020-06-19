e43852af160cdb9f8bf69bce485207fd845a9748
Jerakin <the_rensvind@hotmail.com>
2020-06-19 17:59:20 +0200

    ability id

--- converter/packages/ability.py
+++ converter/packages/ability.py
@@ -1,6 +1,6 @@
 import json
 import converter.util as util
-
+import hashlib
 
 class Ability:
     def __init__(self, name, json_data):
@@ -11,9 +11,13 @@ class Ability:
         if name in util.EXTRA_ABILITY_DATA:
             util.merge(self.output_data, util.EXTRA_ABILITY_DATA[name])
 
+    def set_id(self):
+        self.output_data["_id"] = hashlib.sha256(self.output_data["name"].encode('utf-8')).hexdigest()[:16]
+
     def convert(self, json_data):
         self.output_data["data"]["description"][
             "value"] = f'<p>{json_data["Description"]}</p>'
+        self.set_id()
 
     def save(self, file_path):
         if not file_path.parent.exists():

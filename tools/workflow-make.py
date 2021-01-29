d0a911048d4d4e80e3c944e7f80056cd7a26db5f
Jerakin <the_rensvind@hotmail.com>
2021-01-29 22:49:03 +0100

    Packages being added twice

--- tools/workflow-make.py
+++ tools/workflow-make.py
@@ -31,11 +31,6 @@ import converter.util as util
 main.make()
 
 
-# Update the manifest
-for pack_name, pack_def in foundry.packs.items():
-    if (util.BUILD / pack_name).exists():
-        foundry.module_definition["packs"].append(pack_def)
-
 with (util.PROJECT / "module.json").open("w", encoding="utf-8") as fp:
     json.dump(foundry.module_definition, fp, indent=2, ensure_ascii=False)
 
